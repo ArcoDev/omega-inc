f644d26ef8cbf26945d059974244f97e9e12073a
christian.acosta <osvaldo.acosta@transmontes.com>
2023-10-15 00:45:39 -0600

    agregando sesion archivo 2

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
+    include './includes/sesion.php';
     session_start();
+    var_dump(session_start());
     error_reporting(0);
     $nameUser = $_SESSION['email'];
     if($nameUser === null || $nameUser === '') {

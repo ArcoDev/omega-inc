1b3a597e1af9074c089d9128fcf674c392a5db26
christian.acosta <osvaldo.acosta@transmontes.com>
2023-10-14 23:43:24 -0600

    ajustes alertas validacion

--- includes/controller.php
+++ includes/controller.php
@@ -2,7 +2,7 @@
 
     if(!empty($_POST['btnIngresar'])) {
         if (empty($_POST['email']) && empty($_POST['password'])) {
-            echo '<div class="alert alert-danger mt-2">Todos los campos con obligatorios!!!</div>';
+            echo '<div class="alert alert-danger m-31">Todos los campos con obligatorios!!!</div>';
         } else {
             $email = $_POST['email'];
             $pass = $_POST['password'];
@@ -10,14 +10,14 @@
             if ($data = $sql->fetch_object()) {
                 header("location:index.html");
             } else {
-                echo '<div class="alert alert-danger mt-2">Acceso denegado!!!</div>';
+                echo '<div class="alert alert-danger m-31">Acceso denegado!!!</div>';
             }
         }
     }
 
     if(!empty($_POST['btnRegistrar'])) {
         if (empty($_POST['email']) && empty($_POST['password']) && empty($_POST['name'])) {
-            echo '<div class="alert alert-danger mt-2">Todos los campos con obligatorios!!!</div>';
+            echo '<div class="alert alert-danger m-31">Todos los campos con obligatorios!!!</div>';
         } else {
             $email = $_POST['email'];
             $name = $_POST['name'];

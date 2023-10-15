e28f8522ebbfc836aae022d7da1ed17ce635fa3c
christian.acosta <osvaldo.acosta@transmontes.com>
2023-10-14 23:39:04 -0600

    correcion al guardar usuario

--- includes/controller.php
+++ includes/controller.php
@@ -24,9 +24,9 @@
             $pass = $_POST['password'];
             $sql = $conection->query("INSERT INTO usuarios (correo, nombre, contrasena) VALUES ('$email', '$name', '$pass')");
             if ($sql === true) {
-                echo '<div class="alert alert-success mt-2">Se ha guardado correctamente!!!</div>';
+                echo '<div class="alert alert-success m-3">Se ha guardado correctamente!!!</div>';
             } else {
-                echo '<div class="alert alert-danger mt-2">No se pudo guardar, verifica los datos!!!</div>';
+                echo '<div class="alert alert-danger m-3">No se pudo guardar, verifica los datos!!!</div>';
             }
         }
     }

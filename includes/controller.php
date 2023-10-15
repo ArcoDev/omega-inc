d81c46faad7185335bab48dd40ae3dd69fc77eec
christian.acosta <osvaldo.acosta@transmontes.com>
2023-10-14 23:05:52 -0600

    Generadno formulario de registro

--- includes/controller.php
+++ includes/controller.php
@@ -2,7 +2,7 @@
 
     if(!empty($_POST['btnIngresar'])) {
         if (empty($_POST['email']) && empty($_POST['password'])) {
-            echo '<div class="alert alert-danger">Todos los campos con obligatorios!!!</div>';
+            echo '<div class="alert alert-danger mt-2">Todos los campos con obligatorios!!!</div>';
         } else {
             $email = $_POST['email'];
             $pass = $_POST['password'];
@@ -10,7 +10,7 @@
             if ($data = $sql->fetch_object()) {
                 header("location:index.html");
             } else {
-                echo '<div class="alert alert-danger">Acceso denegado!!!</div>';
+                echo '<div class="alert alert-danger mt-2">Acceso denegado!!!</div>';
             }
         }
     }
---
+++ registro.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 offset-md-3 mt-5">
+                <h2>Registro de Usuarios</h2>
+                <form action="registrar.php" method="POST">
+                    <div class="mb-3">
+                        <label for="correo" class="form-label">Correo Electrónico</label>
+                        <input type="email" class="form-control" id="correo" name="correo" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="nombre" class="form-label">Nombre</label>
+                        <input type="text" class="form-control" id="nombre" name="nombre" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="contrasena" class="form-label">Contraseña</label>
+                        <input type="password" class="form-control" id="contrasena" name="contrasena" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Registrar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Incluimos los scripts de Bootstrap 5 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>

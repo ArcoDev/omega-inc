bd9dc00c6d1061958c70f68e78ae75a652e2ce87
christian.acosta <osvaldo.acosta@transmontes.com>
2023-10-14 23:39:56 -0600

    test

--- registro.php
+++ registro.php
@@ -43,7 +43,7 @@
                     </div>
                 </div>
                 <div class="card-footer text-muted w-100 d-flex justify-content-between">
-                    <button class="btn btn-success"><a class="text-decoration-none text-white" href="index.html">Inicia Sesión</a></button>
+                    <!-- <span class="btn btn-success"><a class="text-decoration-none text-white" href="index.html">Inicia Sesión</a></span> -->
                     <input name="btnRegistrar" type="submit" value="Registrar Usuario" class="btn btn-primary btn-lg">
                     </input>
                 </div>
